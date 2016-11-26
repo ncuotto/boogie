7eca65d1e14e055cd419812430a5b9c8c0e43e73
Bart Olsthoorn <bartolsthoorn@gmail.com>
2016-11-26 14:58:30 +0100

    Update capture.js

--- capture.js
+++ capture.js
@@ -3,6 +3,14 @@ var width, height;
 
 var counts_window = [];
 
+Array.prototype.max = function() {
+  return Math.max.apply(null, this);
+};
+
+Array.prototype.min = function() {
+  return Math.min.apply(null, this);
+};
+
 function setup() {
   width = 320;
   height = 240;
@@ -29,22 +37,34 @@ function draw() {
   // Count the dark pixels for each column
   for (var x = 0; x < width; x++) {
     for (var y = 0; y < height; y++) {
-      var i = y * width + x;
+      var i = y * width*4 + x *4;
       var r = capture.pixels[i];
       var g = capture.pixels[i+1];
       var b = capture.pixels[i+2];
-      if (r > 150 && g > 150 & b > 150)
-        counts[i]++;
+      if (!(r > 100 && g > 100 & b > 100))
+        counts[x]++;
     }
   }
 
   // Debug, plot the histogram
-  for (var x = 0; x < width; x++) {
-    ellipse(x, height-counts[i], 2, 2);
+  // & Find the maximum
+  var max_x = 0;
+  var max_counts = 0;
+  for (var x = 20; x < width-20; x++) {
+    fill(255, 255, 255);
+    ellipse(x, height-counts[x], 5, 5, 1);
+    if (counts[x] > max_counts) {
+      max_counts = counts[x];
+      max_x = x;
+    }
   }
 
+  // Plot the maximum
+  fill(255, 204, 0);
+  ellipse(max_x, height-max_counts, 10, 10, 10);
+
   // Add counts to sliding window
-  counts_window.push([n, counts]);
+  counts_window.push([n, 9999999]);
 
   // Remove counts older than 5 seconds
   for(var i = counts_window.length-1; i >= 0 ; i--){
